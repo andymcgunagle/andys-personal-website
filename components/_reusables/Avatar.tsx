33c91f240b6ff8df4793d157ee0b623065b7d947
andymcgunagle <amcgunagle@gmail.com>
2022-02-23 18:10:20 -0800

    Larger Avatar.

--- components/_reusables/Avatar.tsx
+++ components/_reusables/Avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Avatar({ additionalStyles }: AvatarProps) {
   return (
     <div
-      className={`relative h-36 w-36 rounded-full overflow-hidden m-auto ${additionalStyles}`}
+      className={`relative h-48 w-48 rounded-full overflow-hidden m-auto ${additionalStyles}`}
     >
       <Image
         src="/andy-mcgunagle.jpeg"
